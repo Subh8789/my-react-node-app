59e4203f74befd185ba1e9ca3920244bdb492dc0
subh8789 <subhkumar7352@>
2024-07-30 18:52:53 +0530

    pdpinfinitecall

--- frontend/src/customHook/usePdpApiCall.js
+++ frontend/src/customHook/usePdpApiCall.js
@@ -1,29 +1,34 @@
 import React from "react";
-
 import axios from "axios";
-import Pdp from "../components/Pdp";
 
 function usePdpApiCall(api) {
-    const [pdpData, setPdpData] = React.useState({});
-    const [error, setError] = React.useState(null);
-    React.useEffect(() => {
-        const fetchData = async () => {
-        try {
-            const response = await axios.get(api);
-            setPdpData(response.data);
-            //console.log("response", response);
-            console.log("pdpData", pdpData);
-        } catch (error) {
-            setError(error);
-        }
-        };
-    
-        fetchData();
-    }, [api]);
-    return {
-        pdpData,
-        error
+  const [pdpData, setPdpData] = React.useState({});
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(api);
+        setPdpData((prevData) => {
+          // Only update state if the new data is different
+          if (JSON.stringify(prevData) !== JSON.stringify(response.data)) {
+            return response.data;
+          }
+          return prevData;
+        });
+        console.log("pdpData", response.data);
+      } catch (error) {
+        setError(error);
+      }
     };
+
+    fetchData();
+  }, [api, pdpData]); // pdpData included to satisfy the linter
+
+  return {
+    pdpData,
+    error,
+  };
 }
 
 export default usePdpApiCall;
