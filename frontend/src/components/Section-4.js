b287cf010450658f6c1059a84fa5c3d9ef9caf3a
subh8789 <subhkumar7352@>
2024-07-30 22:27:13 +0530

    css added to imagegrid

--- frontend/src/components/Section-4.js
+++ frontend/src/components/Section-4.js
@@ -1,21 +1,20 @@
 import React from "react";
 
-const ImageGrid = ({imageGrid}) => {
-console.log("imagegrud from comp", imageGrid)
+const ImageGrid = ({ imageGrid }) => {
+  console.log("imagegrud from comp", imageGrid);
   if (!imageGrid) return null;
   return (
     <div className="collage">
-
-    { imageGrid && imageGrid.map((imagegrid,index)=>(
-              <img
-                src={imagegrid?.imagegrid_image[0]?.url}
-                alt={imagegrid?.alttext_for_imagegrid_image}
-                className="collage-image imageGrid?.class_name"
-              />
-    ))
-    }
+      {imageGrid &&
+        imageGrid.map((imagegrid, index) => (
+          <img
+            key={index}
+            src={imagegrid?.imagegrid_image[0]?.url}
+            alt={imagegrid?.alttext_for_imagegrid_image}
+            className={`collage-image ${imagegrid?.class_name}`}
+          />
+        ))}
     </div>
-
   );
 };
 
